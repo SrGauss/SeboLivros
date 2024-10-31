9cd246d568759ebd8eaaaf5c0546df32e3c49f6a
Gauss <ftgustavo0@gmail.com>
2024-10-30 22:34:16 -0300

    novos

--- cadastroLivro.php
+++ cadastroLivro.php
@@ -4,128 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="LogoTrain.png" type="image/x-icon">
+    <link rel="stylesheet" href="StyleCadastroLivro.css">
 
     <title>Cadastro Livro</title>
-
-    <style>
-    body{
-        background-color: #363062;
-    }
-
-    .desc{
-        resize: none;
-    }
-
-    .imgs{
-        font-size: 11.5px;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        top: 3px;
-        left: 0px;
-        color: transparent;
-    }
-
-    .imgs::-webkit-file-upload-button {
-        border-radius: 4px;
-        border: none;
-        position: relative;
-        padding: 7px;
-    }
-
-    .userImg{
-        border: solid 2px black;
-        border-radius: 7px;
-        position: absolute;
-        width: 100px;
-        height: 140px;
-        top: 150px;
-        left: 150px;
-    }
-
-    .userBorder{
-        border: solid 2px black;
-        border-radius: 7px;
-        position: absolute;
-        width: 100px;
-        height: 140px;
-        top: 150px;
-        left: 150px;
-    }
-
-    .back{
-        background-color: #DBD3D3;
-        position: absolute;
-        height: 97.5%;
-        width: 40%;
-        border-radius: 50px;
-        box-shadow: 3px 3px 3px #4D4C7D;
-        user-select: none;
-    }
-
-    input[type="text"], input[type="password"] {
-        padding: 15px;
-        font-size: 16px;
-        border: 1px solid #ccc;
-        border-radius: 15px;
-        transition: 0.5s;
-        position: relative;
-        width: 350px;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, 90%);
-    }
-
-    input[type="number"]{
-        padding: 15px;
-        font-size: 16px;
-        border: 1px solid #ccc;
-        border-radius: 15px;
-        transition: 0.5s;
-        position: relative;
-        width: 200px;
-        top: 50%;
-        left: 50%;
-        transform: translate(-82%, 90%);
-    }
-    
-    input[type="submit"] {
-        background-color: #F99417;
-        color: white;
-        padding: 18px 20px;
-        margin: 7px 0;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-        position: relative;
-        top: -5px;
-    }
-
-    select{
-        padding: 15px;
-        font-size: 16px;
-        border: 1px solid #ccc;
-        border-radius: 15px;
-        transition: 0.5s;
-        position: relative;
-        width: 200px;
-        top: 50%;
-        left: 50%;
-        transform: translate(-94%, 90%);
-    }
-
-    option{
-        padding: 15px;
-        font-size: 16px;
-        border: 1px solid #ccc;
-        border-radius: 15px;
-        transition: 0.5s;
-        position: relative;
-        width: 200px;
-        top: 50%;
-        left: 50%;
-        transform: translate(-94%, 90%);
-    }
-
-    </style>
 
 </head>
 <body>
@@ -169,15 +50,17 @@
             <br>
             <textarea class="desc" placeholder="Descrição" name="descricao" id="descricao"></textarea>
             <br>
-            <input class="imgs" type="file" accept="image/png, image/jpeg" name="image" id="image"/>
+            <label for="image" class="imgs" id="labelImage">Selecionar Imagem</label>
+            <input style="display: none;" type="file" accept="image/png, image/jpeg" name="image" id="image"/>
             <br><br>
-            <input class="imgs" type="file" accept="application/pdf" name="pdf" id="pdf"/>
+            <label class="PDF" for="pdf" id="labelPdf">Selecionar PDF</label>
+            <input style="display: none;" type="file" accept="application/pdf" name="pdf" id="pdf"/>
 
             <input value="Enviar" type="submit">
         </form>
     </div>
 
-    <script>
+<script>
 
         // Função para exibir a imagem selecionada no elemento img
         document.getElementById("image").addEventListener("change", function(event) {
@@ -193,7 +76,28 @@
             }
         });
 
-    </script>
+            // Função para monitorar seleção de arquivos e mudar a cor do texto dos labels
+        function updateLabelColor(inputId, labelId) {
+            const input = document.getElementById(inputId);
+            const label = document.getElementById(labelId);
+
+            input.addEventListener('change', function () {
+                // Muda para verde se um arquivo foi selecionado, cinza se não
+                if (input.files.length > 0) {
+                    label.style.color = 'green';
+                    label.style.textDecoration = 'underline';
+                } else {
+                    label.style.color = 'gray';
+                    label.style.textDecoration = 'none';
+                }
+            });
+        }
+
+        // Aplica a função aos inputs de imagem e PDF
+        updateLabelColor('image', 'labelImage');
+        updateLabelColor('pdf', 'labelPdf');
+
+</script>
 
 
 
